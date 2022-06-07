c9b8d4165297077c762b8613cf6b76521b47ea94
David Israel <davidisrael.rj@gmail.com>
2022-06-07 06:01:02 -0300

    trabalhando com Frame

--- AppGallery/AppGallery/AppBase/Menu.xaml.cs
+++ AppGallery/AppGallery/AppBase/Menu.xaml.cs
@@ -91,5 +91,11 @@ namespace AppGallery.AppBase
             ((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Layout.ControleLayout.Controle();
             ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
         }
+
+        private void AbrirFrame(object sender, EventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Layout.QuadradoLayout.Quadrado();
+            ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+        }
     }
 }
