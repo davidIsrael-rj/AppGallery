ffd416c620ff65b7e66c7059aabe0259449592a6
David Israel <davidisrael.rj@gmail.com>
2022-06-21 21:26:43 -0300

    Trabalhando com Stepper

--- AppGallery/AppGallery/AppBase/Menu.xaml.cs
+++ AppGallery/AppGallery/AppBase/Menu.xaml.cs
@@ -157,6 +157,12 @@ namespace AppGallery.AppBase
             ((MasterDetailPage)App.Current.MainPage).Detail = new NavigationPage(new XamarinForms.Controles.InterruptorControle.Interruptor());
             ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
         }
+
+        private void AbrirStepper(object sender, EventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail = new NavigationPage(new XamarinForms.Controles.PassadorControle.Passador());
+            ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+        }
     }
        
 }
