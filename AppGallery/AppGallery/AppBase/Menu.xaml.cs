37a2a3299e47f4bcf172d0961fc735f21d6fd88a
David Israel <davidisrael.rj@gmail.com>
2022-06-23 06:14:40 -0300

    trabalhando com DatePicker

--- AppGallery/AppGallery/AppBase/Menu.xaml.cs
+++ AppGallery/AppGallery/AppBase/Menu.xaml.cs
@@ -175,6 +175,12 @@ namespace AppGallery.AppBase
             ((MasterDetailPage)App.Current.MainPage).Detail = new NavigationPage(new XamarinForms.Controles.SelecaoControle.Selecao());
             ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
         }
+
+        private void AbrirDatePicker(object sender, EventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail = new NavigationPage(new XamarinForms.Controles.DataSelecaoControle.DataSelecao());
+            ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+        }
     }
        
 }
