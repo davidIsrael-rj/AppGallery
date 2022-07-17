10fad74b2b6776617bcaa293cc32ae15cb34486c
David Israel <davidisrael.rj@gmail.com>
2022-07-17 10:47:57 -0300

    trabalhando com ProgressBar

--- AppGallery/AppGallery/AppBase/Menu.xaml.cs
+++ AppGallery/AppGallery/AppBase/Menu.xaml.cs
@@ -193,6 +193,12 @@ namespace AppGallery.AppBase
             ((MasterDetailPage)App.Current.MainPage).Detail = new NavigationPage(new XamarinForms.Controles.IndicadorAtividadesControle.IndicadoDeAtividades());
             ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
         }
+
+        private void AbrirProgressBar(object sender, EventArgs e)
+        {
+            ((MasterDetailPage)App.Current.MainPage).Detail = new NavigationPage(new XamarinForms.Controles.BarraDeProgessoControle.BarraDeProgresso());
+            ((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+        }
     }
        
 }
---
+++ AppGallery/AppGallery/XamarinForms/Controles/BarraDeProgessoControle/BarraDeProgresso.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppGallery.XamarinForms.Controles.BarraDeProgessoControle
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BarraDeProgresso : ContentPage
+    {
+        double valorProgresBar = 0.0;
+        public BarraDeProgresso()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            valorProgresBar += 0.10; 
+            BarraDeProgresso1.ProgressTo(valorProgresBar, 500, Easing.BounceOut);
+        }
+    }
+}
